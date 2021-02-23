47ad0b12bb352b476b1871491e2b14212ec06a3c
Thejus5 <thejuss@qburst.com>
2021-02-23 16:47:14 +0530

    Create landing page

---
+++ src/Pages/landingPage.js
@@ -0,0 +1,8 @@
+export default function LandingPage(props){
+    return(
+        <>
+            <h1>GitHub Actions</h1>
+            <p>If you are seeing this, that means GH actions is working fine for this repo</p>
+        </>
+    )
+}
